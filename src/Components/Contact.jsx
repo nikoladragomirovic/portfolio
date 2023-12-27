c313a1bf003d3d31d9439e229191d373eb7d23f5
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2023-12-27 17:34:03 +0100

    Contact Page Changes
    - Changed Animation Timing
    - Changed Layout
    - Added Hover Effects on Social Media Icons

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -11,53 +11,40 @@ const Contact = () => {
 
   return (
     <>
-      <h1 className="w-full flex items-center justify-center mt-16 text-6xl text-indigo-500 font-rubikMaps font-bold animate__animated animate__bounceInDown">
+      <h1 className="w-full flex items-center justify-center mt-16 mb-20 text-6xl text-indigo-500 font-rubikMaps font-bold animate__animated animate__bounceInDown">
         CONTACT
       </h1>
-      <div className="flex w-full flex-row items-center justify-center my-20">
-        <span className="p-4 rounded-lg bg-indigo-500">
-          <ImGithub className="text-4xl text-indigo-100 animate__animated animate__zoomInDown animate-delay-100" />
-        </span>
-        <span className="p-4 rounded-lg bg-indigo-500 mx-24">
-          <RiLinkedinLine className="text-4xl text-indigo-100 animate__animated animate__zoomInDown animate-delay-200" />
-        </span>
-        <span className="p-4 rounded-lg bg-indigo-500">
-          <RxInstagramLogo className="text-4xl text-indigo-100 animate__animated animate__zoomInDown animate-delay-300" />
-        </span>
-      </div>
       <div className="w-full flex flex-col items-center justify-center px-10 pb-10">
         <div className="w-7/12 flex flex-row z-20 items-center justify-center">
           <input
             key={comebackKey}
             placeholder="Your e-mail address"
-            className={`w-full py-4 mb-2 rounded-xl bg-indigo-50 duration-500 outline px-3 outline-1  placeholder-indigo-300 font-poppins text-indigo-500 animate__animated ${
+            className={`w-full py-4 rounded-xl z-30 bg-indigo-50 duration-500 outline px-3 outline-1  placeholder-indigo-300 font-poppins text-indigo-500  ${
               animateSend
-                ? "animate__bounceOutRight animate-delay-700"
-                : "animate__animated animate__bounceInLeft animate-delay-400 outline-indigo-500"
+                ? "animate__animated animate__bounceOutRight animate-delay-1000"
+                : "animate__animated animate__bounceInLeft outline-indigo-500 animate-delay-[600ms]"
             }`}
             readOnly={animateSend}
           ></input>
           <BiSolidSend
             onClick={() => {
-              setTimeout(() => {
-                setAnimateSend(true);
-              }, 0);
+              setAnimateSend(true);
               setTimeout(() => {
                 setAnimateSend(false);
                 setComebackKey(comebackKey + 1);
               }, 2200);
             }}
-            className={`text-indigo-500 mb-2 text-5xl animate__animated mr-5 ml-8 ${
+            className={`text-indigo-500 text-5xl animate__animated mx-5 ${
               animateSend
-                ? "animate__bounceOutRight animate-delay-700"
-                : "animate__animated animate__bounceInLeft animate-delay-200"
+                ? "animate__bounceOutRight animate-delay-1000"
+                : "animate__animated animate__bounceInLeft animate-delay-[600ms]"
             }`}
           />
         </div>
         <p
           className={`text-indigo-500 duration-300 text-light font-light text-xl ${
             animateSend
-              ? "opacity-100 delay-[1100ms] translate-y-32"
+              ? "opacity-100 delay-[1400ms] translate-y-32"
               : "opacity-0 -mb-3"
           }`}
         >
@@ -66,12 +53,29 @@ const Contact = () => {
         <textarea
           placeholder="Your message"
           readOnly={animateSend}
-          className={`w-7/12 pb-64 duration-500 rounded-2xl outline p-3 resize-none outline-1 placeholder-indigo-300  font-poppins text-indigo-500 animate__animated bg-indigo-50 ${
+          className={`w-7/12 pb-64 duration-500 rounded-2xl outline z-10 p-3 resize-none outline-1 placeholder-indigo-300  font-poppins text-indigo-500 animate__animated bg-indigo-50 ${
             animateSend
               ? "animate__animated animate__zoomOutUp"
-              : "animate__animated animate__zoomInDown animate-delay-[800ms] outline-indigo-500"
+              : "animate__animated animate__zoomInDown animate-delay-[1000ms] outline-indigo-500"
           }`}
         ></textarea>
+        <div className="flex w-full flex-row items-center justify-center mt-20">
+          <div className="animate__animated animate__zoomInDown animate-delay-[1200ms] flex">
+            <span className="group p-2 rounded-lg bg-indigo-200 hover:rotate-[23deg] duration-300 hover:translate-y-1">
+              <ImGithub className="text-4xl text-indigo-500 group-hover:-translate-x-7 group-hover:-rotate-[32deg] duration-300" />
+            </span>
+          </div>
+          <div className="animate__animated animate__zoomInDown animate-delay-[1400ms] flex">
+            <span className="group p-2 rounded-lg bg-indigo-200 hover:rotate-[23deg] duration-300 hover:translate-y-1 mx-36">
+              <RiLinkedinLine className="text-4xl text-indigo-500 group-hover:-translate-x-7 group-hover:-rotate-[32deg] duration-300" />
+            </span>
+          </div>
+          <div className="animate__animated animate__zoomInDown animate-delay-[1600ms] flex">
+            <span className="group p-2 rounded-lg bg-indigo-200 hover:rotate-[23deg] duration-300 hover:translate-y-1">
+              <RxInstagramLogo className="text-4xl text-indigo-500 group-hover:-translate-x-7 group-hover:-rotate-[32deg] duration-300" />
+            </span>
+          </div>
+        </div>
       </div>
     </>
   );
